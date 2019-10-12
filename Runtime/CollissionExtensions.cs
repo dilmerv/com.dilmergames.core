d109879a2d24aab0fcd8deae2062704e2403bb9a
Dilmer Valecillos <dilmervst@gmail.com>
2019-10-12 07:49:38 -0600

    Added singletons

--- Runtime/CollissionExtensions.cs
+++ Runtime/CollissionExtensions.cs
@@ -7,16 +7,6 @@ namespace DilmerGames.Core
 {
     public class CollissionExtensions : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+       
     }
 }
--- Runtime/GameUtilities.cs
+++ Runtime/GameUtilities.cs
@@ -6,8 +6,6 @@ namespace DilmerGames.Core
 {
     public class GameUtilities : MonoBehaviour
     {
-        public static string Version = "1.0.0";
-
-        public static string SomethingElse = "Something Else";
+        
     }
 }
