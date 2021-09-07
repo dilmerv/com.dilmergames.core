a5be0a5f54930055c2561cd91ed0eaa570cfca5a
Dilmer Valecillos <dilmervst@gmail.com>
2021-09-07 16:27:33 -0600

    Added logger and removed not implemented classes

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,6 +1,6 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace DilmerGames.Core
+namespace DilmerGames.Core.Singletons
 {
     public class Singleton<T> : MonoBehaviour
         where T : Component
